6a5a753fc70818e3318842655035ef6eab6b61aa
supernova0730 <kuanysheveldar123@gmail.com>
2022-06-04 19:04:01 +0600

    added log to gorm

--- pkg/client/postgres/postgres.go
+++ pkg/client/postgres/postgres.go
@@ -3,7 +3,10 @@ package postgres
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
+	"log"
+	"os"
 )
 
 func Connect(conf Config) (*gorm.DB, error) {
@@ -11,8 +14,17 @@ func Connect(conf Config) (*gorm.DB, error) {
 		DSN: conf.BuildDSN(),
 	})
 
+	gormLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			Colorful: true,
+			LogLevel: logger.Info,
+		},
+	)
+
 	gormConfig := &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{SingularTable: true},
+		Logger:         gormLogger,
 	}
 
 	return gorm.Open(gormDialector, gormConfig)
